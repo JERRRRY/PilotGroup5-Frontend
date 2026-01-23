095b966b21d9989b65f7bc3f6280a0bf54b2c28f
Haoyu Li <111718956+Haoyu-L@users.noreply.github.com>
2026-01-23 14:33:48 -0500

    remove homepage video count

--- frontend/src/components/home/CourseCard.tsx
+++ frontend/src/components/home/CourseCard.tsx
@@ -9,7 +9,8 @@ const CourseCard = ({ course }: CourseCardProps) => {
   // 如果没有 pages 数组，默认为 0
   // 注意：这依赖于后端 getAllCourses 是否返回了 videoCount。
   // 如果后端没算，这里就临时算一下或者显示 0。
-  const videoCount = course.pages ? course.pages.filter(p => p.type === 'video').length : 0;
+  // const videoCount = course.pages ? course.pages.filter(p => p.type === 'video').length : 0;
+  // const pageCount = course.pages.length;
 
   return (
     <Link to={`/course/${course._id}`} className="block group h-full">
@@ -54,17 +55,17 @@ const CourseCard = ({ course }: CourseCardProps) => {
           </p>
 
           {/* Footer Stats */}
-          <div className="pt-4 border-t border-slate-100 flex items-center justify-between text-xs font-medium text-slate-400">
+          {/* <div className="pt-4 border-t border-slate-100 flex items-center justify-between text-xs font-medium text-slate-400">
             <div className="flex items-center gap-1.5">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 text-violet-500">
                  <path d="M11.25 4.533A9.707 9.707 0 006 3.75a9.753 9.753 0 00-5.929 1.509c-.369.238-.485.723-.26 1.09l.805 1.306c.213.346.656.47.998.304a8.23 8.23 0 012.953-.829c.45-.044.903-.021 1.351.064l.903 1.467c.27.44.775.663 1.28.534A9.733 9.733 0 0012 8.75a9.733 9.733 0 002.949.479c.506.129 1.01.394 1.28.534l.903-1.467c.449-.085.902-.108 1.352-.064.995.097 1.968.382 2.952.829.342.166.786.042.998-.304l.806-1.306c.225-.367.109-.852-.26-1.09A9.753 9.753 0 0018 3.75c-1.83 0-3.577.27-5.25.783-.483-.148-.98-.233-1.5-.233zm-8.205 8.402A8.25 8.25 0 0112 20.25a8.25 8.25 0 01-8.955-7.315zm17.91 0A8.25 8.25 0 0012 20.25a8.25 8.25 0 008.955-7.315zM12 10.5a3 3 0 110 6 3 3 0 010-6z" />
               </svg>
-              <span>{videoCount} Videos</span>
-            </div>
+              <span>{pageCount} Pages</span>
+            </div> */}
             
             {/* 也可以加个 ID 或者 讲师名 */}
             {/* <span>ID: {course._id.slice(-4)}</span> */}
-          </div>
+          {/* </div> */}
         </div>
       </div>
     </Link>
